79ef60c226a623e08ad158e9ef22302eb2da2e71
Bao Bui <bvqbao@gmail.com>
2020-02-14 14:26:06 +0100

    Add a comment.

--- xen/arch/x86/mm.c
+++ xen/arch/x86/mm.c
@@ -4164,6 +4164,8 @@ int xenmem_add_to_physmap_one(
     if ( !rc )
     {
         rc = guest_physmap_add_page(d, gpfn, mfn, PAGE_ORDER_4K);
+
+        /* Test shared memory buffer */
         if ( space == XENMAPSPACE_pml_shared_info )
             d->vtf_info.pml[0] = 69;
     }
